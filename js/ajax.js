70a9920e5fb836127d6859866f73bcb2ff90182c
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2016-06-22 10:55:38 +0200

    Added the basic ajax handler and input fields for dates

---
+++ js/ajax.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $("#submit").click(function() {
+    var location = $("#address").val();
+    var start_date = $("#start_date").val();
+    var end_date = $("#end_date").val();
+
+    //
+    $.ajax({
+      type: "POST",
+      // url: "../../cgi-bin/test.cgi",
+      // change to relative URL in production
+      url: "https://www.wetter.space/cgi-bin/jsonGenerator.cgi",
+      data: {
+        location:location,
+        start_date:start_date,
+        end_date:end_date,
+      },
+      success: function(content) {
+        console.log(content);
+      }
+    });
+  });
+});
