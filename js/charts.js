032ba00f692f90e01c18d66413f3bfedbaf9d91e
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2016-07-07 12:28:22 +0200

    Added Drag and Drop feature, not final

--- js/charts.js
+++ js/charts.js
@@ -1,3 +1,5 @@
+var chartsDataTables = [];
+
 function drawDashboard(responseData) {
   var element  = responseData[0]["element"];
   element = element.replace(/ae/g,"ä").replace(/oe/g,"ö").replace(/ue/g,"ü");
@@ -8,6 +10,9 @@ function drawDashboard(responseData) {
   var dashboardId = createDashboardAndGetId(element);
   var elementChartId = element + "_chart";
   var elementSliderId = element + "_slider";
+
+  chartsDataTables[elementChartId] = [];
+  chartsDataTables[elementChartId] = [responseData];
 
   addDivForChart(elementChartId, dashboardId);
   addDivForSlider(elementSliderId, dashboardId);
@@ -23,7 +28,11 @@ function drawDashboard(responseData) {
             'containerId': elementSliderId,
             'options': {
               "filterColumnIndex" : 0,
-
+              "ui" : {
+                "chartOptions" : {
+                  "height" : 50
+                }
+              }
             }
           });
   var lineChart = new google.visualization.ChartWrapper({
@@ -80,10 +89,10 @@ function addDivForChart(element, dashboardId) {
   var div = document.createElement("div");
   var dashboardDiv = document.getElementById(dashboardId);
   div.setAttribute("id", element);
-  // div.setAttribute("draggable", "true");
-  // div.setAttribute("ondragstart", "dragStart(event)");
-  // div.setAttribute("ondrop", "dragDrop(event)");
-  // div.setAttribute("ondragover", "allowDrop(event)");
+  div.setAttribute("draggable", "true");
+  div.setAttribute("ondragstart", "dragStart(event)");
+  div.setAttribute("ondrop", "dragDrop(event)");
+  div.setAttribute("ondragover", "allowDrop(event)");
   dashboardDiv.appendChild(div);
 }
 
@@ -100,12 +109,101 @@ function dragStart(e) {
 }
 
 function dragDrop(e) {
-  var draggedItemId = e.dataTransfer.getData("Text");
-  console.log(draggedItemId);
-  console.log(e.currentTarget.getAttribute("id"));
-  // alert(e.currentTarget.getAttribute("id"));
+  var originChartId = e.dataTransfer.getData("Text");
+  var targetChartId = e.currentTarget.getAttribute("id");
+  createOverlayChart(originChartId, targetChartId);
 }
 
 function allowDrop(e) {
   e.preventDefault();
+}
+
+function createOverlayChart(originChartId, targetChartId) {
+  var originData = chartsDataTables[originChartId];
+  var targetData = chartsDataTables[targetChartId];
+  var allResponseData = originData.concat(targetData);
+  var targetBasicId = targetChartId.replace(/_chart/g, "").replace(/_overlay/g, "");
+  var newBasicId = (originChartId + " " + targetChartId).replace(/_chart/g, "").replace(/_overlay/g, "");
+
+  var dataArray = getOverlayDataArray(allResponseData);
+
+  var targetChartElement = document.getElementById(targetChartId);
+  var targetSliderElement = document.getElementById(targetBasicId + "_slider");
+  if (targetSliderElement == null) {
+    targetSliderElement = document.getElementById(targetBasicId + "_slider_overlay");
+  }
+  var targetDashboardElement = targetChartElement.parentNode;
+
+  uncheckButtons(targetBasicId);
+
+  targetDashboardElement.setAttribute("id", newBasicId + "_dashboard_overlay");
+  targetSliderElement.setAttribute("id", newBasicId + "_slider_overlay");
+  targetChartElement.setAttribute("id", newBasicId + "_chart_overlay")
+
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'X');
+  for (var i = 0; i < allResponseData.length; i++) {
+    data.addColumn("number", allResponseData[i][0]["element"] + " in " + allResponseData[i][0]["einheit"])
+  }
+  data.addRows(dataArray);
+
+  var dashboard = new google.visualization.Dashboard(targetDashboardElement);
+  var dateSlider = new google.visualization.ControlWrapper({
+            'controlType': 'ChartRangeFilter',
+            'containerId': newBasicId + "_slider_overlay",
+            'options': {
+              "filterColumnIndex" : 0,
+              "ui" : {
+                "chartOptions" : {
+                  "height" : 50
+                }
+              }
+            }
+          });
+  var lineChart = new google.visualization.ChartWrapper({
+                  'chartType': 'LineChart',
+                  'containerId': newBasicId + "_chart_overlay",
+                  'options': {
+                    "title": "overlay",
+                    "hAxis": {
+                      "title": 'Datum'
+                    },
+                  }
+                });
+  dashboard.bind(dateSlider, lineChart);
+  dashboard.draw(data);
+
+  //Creates new...or overwrite?
+  chartsDataTables[newBasicId + "_chart_overlay"] = [];
+  chartsDataTables[newBasicId + "_chart_overlay"] = allResponseData;
+}
+
+function getOverlayDataArray(data) {
+  var result = [];
+  for (var i = 0; i < data.length; i++) {
+    var currentDataset = data[i];
+    for(x = 0; x < data[i].length; x++) {
+      var date = currentDataset[x]["date"];
+      date = convertToDate(date);
+
+      var value = currentDataset[x]["wert"];
+      value = parseFloat(value.replace(",", "."));
+
+      if(i == 0) {
+        result[x] = [];
+        result[x][0] = date;
+        result[x][1] = value;
+      } else {
+        result[x][i + 1] = value;
+      }
+    }
+  }
+
+  return result;
+}
+
+function uncheckButtons(id) {
+  var element = document.getElementById(id + "_button");
+  if(element != null) element.checked = false;
 }
